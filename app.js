c2715fa65475a3a5df59f8b8b2e05b3909aefe32
ThewBear <26767607+ThewBear@users.noreply.github.com>
2021-08-09 11:16:39 +0000

    Prettier semicolon

--- app.js
+++ app.js
@@ -1,45 +1,45 @@
-import express from "express"
-import { chromium } from "playwright"
+import express from "express";
+import { chromium } from "playwright";
 
-const browser = await chromium.launch()
+const browser = await chromium.launch();
 const context = await browser.newContext({
   deviceScaleFactor: 2.5,
   viewport: { width: 1200, height: 628 },
-})
+});
 
-const app = express()
-const port = 8080
+const app = express();
+const port = 8080;
 
-const siteWhitelist = ["www.thewdhanat.com"]
+const siteWhitelist = ["www.thewdhanat.com"];
 
 app.get("/:site", async (req, res) => {
-  const urlPath = req.query.path
-  const site = req.params.site
+  const urlPath = req.query.path;
+  const site = req.params.site;
   if (!siteWhitelist.includes(site) || !urlPath || !urlPath.startsWith("/")) {
-    return res.status(403).end()
+    return res.status(403).end();
   }
-  const page = await context.newPage()
-  const url = `https://${site}${urlPath}`
-  const response = await page.goto(url).catch(() => {})
+  const page = await context.newPage();
+  const url = `https://${site}${urlPath}`;
+  const response = await page.goto(url).catch(() => {});
   if (!response || response.status() !== 200) {
-    return res.status(404).end()
+    return res.status(404).end();
   }
-  res.setHeader("Content-Type", `image/png`)
+  res.setHeader("Content-Type", `image/png`);
   res.setHeader(
     "Cache-Control",
     `public, no-transform, s-maxage=86400, max-age=0`
-  )
-  res.setHeader("x-size", JSON.stringify(page.viewportSize()))
-  const file = await page.screenshot()
-  await page.close()
-  console.log(Date(), url)
-  return res.end(file)
-})
+  );
+  res.setHeader("x-size", JSON.stringify(page.viewportSize()));
+  const file = await page.screenshot();
+  await page.close();
+  console.log(Date(), url);
+  return res.end(file);
+});
 
 app.all("/*", (req, res) => {
-  res.status(400).end()
-})
+  res.status(400).end();
+});
 
 app.listen(port, () => {
-  console.log(`og-image app listening at http://localhost:${port}`)
-})
+  console.log(`og-image app listening at http://localhost:${port}`);
+});
